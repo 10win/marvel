756b9947ec2060d1f986d7a68122014210240585
Nik <10winfiles@gmail.com>
2022-10-26 16:13:19 +0300

    the seventh part of the course: a rest application is created that takes data from the api marvel

---
+++ src/services/MarvelService.js
@@ -0,0 +1,36 @@
+class MarvelService {
+    _apiBase = "https://gateway.marvel.com:443/v1/public/";
+    _apiKey = "apikey=3c0f4ff0f36ee3d06aa224256311e65b";
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if(!res.ok){
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const result = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return result.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const result = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(result.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        }
+    }
+}
+
+export default MarvelService;
